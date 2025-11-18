bfb075b503d58337849a83c4212690a97d185db5
Mika <baezamika@gmail.com>
2025-11-18 19:10:56 +0100

    init cd/ci

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:24-alpine as front-builder
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install --frozen-lockfile
+
+COPY . .
+
+RUN npm run build
+
+FROM node:24-alpine as front-runner
+
+WORKDIR /app
+
+COPY --from=front-builder /app/.output ./
+EXPOSE 3000
+
+ENV HOST=0.0.0.0 PORT=3000 NODE_ENV=production
+
+ENTRYPOINT ["npm", "run", "start"]
--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,42 @@
+'use client'
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  
+  const totSecInADay = 24*3600
+  const totSec10InADay = 10*100*100
+
+  function getTime10() {
+    const date = new Date()
+    const currSec = date.getHours()*3600 + date.getMinutes()*60 + date.getSeconds()
+    const currSec10 = currSec * totSec10InADay/totSecInADay
+    const hour10 = Math.floor(currSec10/(100*100))
+    const min10 = Math.floor((currSec10 - hour10*100*100)/100)
+    const sec10 = Math.floor((currSec10 - hour10*100*100 - min10*100))
+    return `${hour10.toString().padStart(2,'0')}:${min10.toString().padStart(2,'0')}:${sec10.toString().padStart(2,'0')}`
+  }
+
+  const [printedTime, setPrintedTime] = useState(getTime10())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+    setPrintedTime(getTime10())
+    }, 1000);
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-16 px-16 bg-white dark:bg-black">
+        <span className="flex flex-col md:flex-row text-8xl gap-24">
+          {printedTime.split(':').map((char, idx) => {
+            return <div key={idx} className="w-36 text-center md:text-start">{char}</div>
+          })}
+        </span>
       </main>
     </div>
   );
